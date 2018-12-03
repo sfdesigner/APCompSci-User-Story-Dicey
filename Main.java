3b88cfb25f57f415105655eae548e8122b8a8b02
Doug <doug@sfdesignerdw.com>
2018-12-02 17:46:20 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
         // initialize the random number generator
         rand = new Random();
-        
+
         // output the result of a roll, with inline rolling and combo testing
         System.out.printf("A pair %s rolled.",checkRoll(rollDie(),rollDie(),rollDie()) ? "was" : "was not");
     }
 
     /**
-     * Method to check the 
+     * Method to check if three dice contain a pair of matching values
      * @param a int; die one
      * @param b int; die two
      * @param c int; die three
@@ -36,11 +36,11 @@ public class Main {
     public static int rollDie() {
         // generate a virtual 6d die roll
         int x = rand.nextInt(7);
-        
+
         // output die roll
         System.out.printf("Die: %d\n",x);
-        
-        // return value of die roll
+
+        // return value of die rolla
         return x;
     }
 }
